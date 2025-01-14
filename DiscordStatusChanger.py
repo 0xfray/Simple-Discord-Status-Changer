e35cb7a33588d44fbf96850a763be67740067fb3
0xfray <0xfray@proton.me>
2025-01-14 21:46:22 +1100

    Add files via upload

---
+++ DiscordStatusChanger.py
@@ -0,0 +1,41 @@
+import time
+import requests
+
+
+url = "https://discord.com/api/v8/users/@me/settings"
+
+
+with open("messages.txt", "r") as file:
+    lines = file.readlines()
+
+def changestatus(message):
+
+    header = {
+        "authorization": "" 
+    }
+
+
+    jsonData = {
+        "status": "dnd", 
+        "custom_status": {
+            "text": message 
+        }
+    }
+
+    try:
+
+        response = requests.patch(url, headers=header, json=jsonData)
+        if response.status_code == 200:
+            print(f"Status changed to: {message}")
+        else:
+            print(f"Failed to change status: {response.status_code} - {response.text}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+while True:
+    for line in lines:
+        message = line.strip()  
+        if message:  
+            changestatus(message)
+            time.sleep(4)
